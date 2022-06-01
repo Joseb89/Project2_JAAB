ce817c0d22f7794ef80cf8a03575ee31485268d8
Joseph Barr <jbarr89@yahoo.com>
2022-06-01 10:04:59 -0500

    Updated patient services.

--- patient_api/src/main/java/com/jaab/revature/service/PatientService.java
+++ patient_api/src/main/java/com/jaab/revature/service/PatientService.java
@@ -20,7 +20,7 @@ public class PatientService {
 
     @Autowired
     public PatientService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("http://project2main:8081").build();
+        this.webClient = webClientBuilder.baseUrl("http://project2main-service:8081").build();
     }
 
     @Autowired
